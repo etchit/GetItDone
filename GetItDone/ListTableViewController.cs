0da3b164f071a494fab3ee9323d503516dea1341
Erin Abrams <erinjabrams@gmail.com>
2018-07-24 18:06:41 +1000

    - added in view controller class for ListTableViewController

---
+++ GetItDone/ListTableViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace GetItDone
+{
+    public partial class ListTableViewController : UITableViewController
+    {
+        public ListTableViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
