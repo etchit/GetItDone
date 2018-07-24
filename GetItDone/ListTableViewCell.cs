16027e44cb335b95f343f477b12774c7c1262195
Erin Abrams <erinjabrams@gmail.com>
2018-07-24 18:08:37 +1000

    - created tableviewcell class for reusable cells in table

---
+++ GetItDone/ListTableViewCell.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Foundation;
+using UIKit;
+
+namespace GetItDone
+{
+    public partial class ListTableViewCell : UITableViewCell
+    {
+        public static readonly NSString Key = new NSString("ListTableViewCell");
+        public static readonly UINib Nib;
+
+        static ListTableViewCell()
+        {
+            Nib = UINib.FromName("ListTableViewCell", NSBundle.MainBundle);
+        }
+
+        protected ListTableViewCell(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+    }
+}
