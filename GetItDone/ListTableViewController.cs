b7aa69eedcc43ef69c06bebb5ddf127cc0bbbd88
Erin Abrams <erinjabrams@gmail.com>
2018-07-29 11:09:41 +1000

    - Added in button on ListViewController to handle adding a new list item - Connected outlet to View Controller - Created Event handler for button press - Added in simple alertview to test button click event was firing

--- GetItDone/ListTableViewController.cs
+++ GetItDone/ListTableViewController.cs
@@ -9,5 +9,21 @@ namespace GetItDone
         public ListTableViewController (IntPtr handle) : base (handle)
         {
         }
+
+        partial void AddListItemWasPressed(UIBarButtonItem sender)
+        {
+
+            Console.WriteLine("Button Was Pressed");
+
+            UIAlertView alert = new UIAlertView()
+            {
+                Title = "alert title",
+                Message = "this is a simple alert"
+            };
+            alert.AddButton("OK");
+            alert.Show();
+
+            //throw new NotImplementedException();
+        }
     }
 }
--- GetItDone/ListTableViewController.designer.cs
+++ GetItDone/ListTableViewController.designer.cs
@@ -14,8 +14,20 @@ namespace GetItDone
     [Register ("ListTableViewController")]
     partial class ListTableViewController
     {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIBarButtonItem AddListItem { get; set; }
+
+        [Action ("AddListItemWasPressed:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void AddListItemWasPressed (UIKit.UIBarButtonItem sender);
+
         void ReleaseDesignerOutlets ()
         {
+            if (AddListItem != null) {
+                AddListItem.Dispose ();
+                AddListItem = null;
+            }
         }
     }
 }
