22fa88275886cf1a2470224d42cea9201c39c7c0
Erin Abrams <erinjabrams@gmail.com>
2018-08-01 15:22:03 +1000

    - Added in ViewWillLayoutSubviews function - Added in popupbox view

--- GetItDone/AddItemAlertView.cs
+++ GetItDone/AddItemAlertView.cs
@@ -8,6 +8,7 @@ namespace GetItDone
 {
     public partial class AddItemAlertView : UIViewController
     {
+
         public AddItemAlertView(IntPtr handle) : base(handle)
         {
         }
@@ -16,12 +17,44 @@ namespace GetItDone
 
         public override void ViewDidLoad()
         {
-            setupUI();
+           // setupUI();
+        }
+
+
+        public override void ViewWillLayoutSubviews()
+        {
+            base.ViewWillLayoutSubviews();
+
+            this.View.BackgroundColor = UIColor.FromRGB(253, 81, 201);
+            UIView popupBox = new UIView(new CGRect(0, 0, (UIScreen.MainScreen.Bounds.Width - 40),(UIScreen.MainScreen.Bounds.Height - 280)));
+            popupBox.Center = View.Center;
+            popupBox.BackgroundColor = UIColor.White;
+            popupBox.Layer.CornerRadius = 5;
+
+        
+            View.AddSubview(popupBox);
         }
 
         private void setupUI(){
-            
-            this.View.BackgroundColor = UIColor.FromRGB(253, 81, 201);
+
+            // Get the parent view's layout
+            var margins = View.LayoutMarginsGuide;
+
+           
+
+           
+
+            //popupBox.BackgroundColor = UIColor.White;
+
+            ////// Pin the leading edge of the view to the margin
+            //NSLayoutConstraint.Create(popupBox, NSLayoutAttribute.Leading, NSLayoutRelation.Equal, View, NSLayoutAttribute.LeadingMargin, 1.0f, 0.0f).Active = true;
+           
+            ////// Pin the trailing edge of the view to the margin
+            //NSLayoutConstraint.Create(popupBox, NSLayoutAttribute.Trailing, NSLayoutRelation.Equal, View, NSLayoutAttribute.TrailingMargin, 1.0f, 0.0f).Active = true;
+           
+            ////// Give the view a 1:2 aspect ratio
+            //NSLayoutConstraint.Create(popupBox, NSLayoutAttribute.Height, NSLayoutRelation.Equal, popupBox, NSLayoutAttribute.Width, 2.0f, 0.0f).Active = true;
+
         }
     }
 }
