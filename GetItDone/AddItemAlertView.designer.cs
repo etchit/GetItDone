cc1954de1919a56ac0625680997571bb5a281f91
Erin Abrams <erinjabrams@gmail.com>
2018-08-06 21:39:49 +1000

    - laid out components of alertview using auto layout constraints

--- GetItDone/AddItemAlertView.designer.cs
+++ GetItDone/AddItemAlertView.designer.cs
@@ -14,8 +14,69 @@ namespace GetItDone
     [Register ("AddItemAlertView")]
     partial class AddItemAlertView
     {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton AddButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton CancelBtn { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIView containerView { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIDatePicker DueDate { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextView TaskDescription { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField TaskTitle { get; set; }
+
+        [Action ("AddItemBtn_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void AddItemBtn_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("CancelBtn_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void CancelBtn_TouchUpInside (UIKit.UIButton sender);
+
         void ReleaseDesignerOutlets ()
         {
+            if (AddButton != null) {
+                AddButton.Dispose ();
+                AddButton = null;
+            }
+
+            if (CancelBtn != null) {
+                CancelBtn.Dispose ();
+                CancelBtn = null;
+            }
+
+            if (containerView != null) {
+                containerView.Dispose ();
+                containerView = null;
+            }
+
+            if (DueDate != null) {
+                DueDate.Dispose ();
+                DueDate = null;
+            }
+
+            if (TaskDescription != null) {
+                TaskDescription.Dispose ();
+                TaskDescription = null;
+            }
+
+            if (TaskTitle != null) {
+                TaskTitle.Dispose ();
+                TaskTitle = null;
+            }
         }
     }
 }
