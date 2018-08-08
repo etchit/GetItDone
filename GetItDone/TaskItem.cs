617f006fbc07b665ebd8e9801f04de599d825597
Erin Abrams <erinjabrams@gmail.com>
2018-08-08 16:41:16 +1000

    - Added in TaskItem class - Added SQLite package for data persistence

---
+++ GetItDone/TaskItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UIKit;
+using SQLite;
+using Foundation;
+
+namespace GetItDone
+{
+    public class TaskItem
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string TaskTitle { get; set; }
+        public string TaskDescription { get; set; }
+        public DateTime TaskDueDate { get; set; }
+        public bool TaskCompleted { get; set; }
+
+        public TaskItem()
+        {
+        }
+
+        public TaskItem(string taskTitle, string taskDesription, DateTime taskDuedate, bool taskCompleted)
+        { 
+            TaskTitle = taskTitle;
+            TaskDescription = taskDesription;
+            TaskDueDate = taskDuedate;
+            TaskCompleted = taskCompleted;
+        }
+    }
+}
