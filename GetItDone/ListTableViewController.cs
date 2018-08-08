d8d7bb3cc2abf9267f808b530696c55a2a109a16
Erin Abrams <erinjabrams@gmail.com>
2018-08-08 16:49:58 +1000

    - added in code to handle the loading of data from db into tableview controller - removed obsolete custom cell

--- GetItDone/ListTableViewController.cs
+++ GetItDone/ListTableViewController.cs
@@ -1,22 +1,88 @@
 using Foundation;
 using System;
+using System.Collections.Generic;
 using UIKit;
+using SQLite;
+using System.IO;
 
 namespace GetItDone
 {
     public partial class ListTableViewController : UITableViewController
     {
-        public ListTableViewController (IntPtr handle) : base (handle)
+
+        private string pathToDB;
+
+        // create list to hold all the to do items
+        private List<TaskItem> taskItems;
+
+        // set cell identifier to match result in Main.Storyboard
+        string cellIdentifier = "Task";
+
+        public ListTableViewController(IntPtr handle) : base(handle)
         {
-            
+            taskItems = new List<TaskItem>();
         }
 
-        //partial void AddListItemWasPressed(UIBarButtonItem sender)
-        //{
+        // display the number of rows based on how many items in <List>TaskItems
+        public override nint RowsInSection(UITableView tableView, nint section)
+        {
+            return taskItems.Count;
+        }
 
-        //    Console.WriteLine("Button Was Pressed");
-        //    //throw new NotImplementedException();
-        //}
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            // make cell reusuable
+            UITableViewCell cell = tableView.DequeueReusableCell(cellIdentifier);
 
+            // specifiy the data that will load in the cells
+            var data = taskItems[indexPath.Row];
+            cell.TextLabel.Text = data.TaskTitle;
+            cell.DetailTextLabel.Text = data.TaskDueDate.ToString();
+
+            return cell;
+        }
+
+
+        //call this method the first time the view loads only 
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            var docFolder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            pathToDB = Path.Combine(docFolder, "GetItDoneDB.db");
+
+            using (var connection = new SQLite.SQLiteConnection(pathToDB))
+            {
+                // table will only be "created" the first time the view is loaded
+               connection.CreateTable<TaskItem>();
+            }
+       
+        }
+
+        public override void ViewDidAppear(bool animated)
+        {
+            base.ViewDidAppear(animated);
+
+            taskItems = new List<TaskItem>();
+
+            using (var connection = new SQLite.SQLiteConnection(pathToDB))
+            {
+                // table will be reloaded each time the view appears
+                var query = connection.Table<TaskItem>();
+
+                // add one item in TaskItem list per row
+                foreach (TaskItem taskItem in query)
+                {
+                    taskItems.Add(taskItem);
+                    TableView.ReloadData();
+                }
+            }
+
+        }
+
+        public override nint NumberOfSections(UITableView tableView)
+        {
+            return 1;
+        }
     }
 }
